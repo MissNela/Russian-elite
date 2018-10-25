f643c2204b04b3b1afb4cbdb490b8edee8d6b882
MissNela <44345612+MissNela@users.noreply.github.com>
2018-10-25 20:51:07 +0200

    Update Russian-elite.py

--- Russian-elite.py
+++ Russian-elite.py
@@ -19,4 +19,5 @@ async def ping():
 	await bot.say(':ping_pong: Why are you trying to ping me??')
 	await bot.say('Dont try it.. You dont have any chance. Heheh')
   
-client.run(os.getnv('TOKEN'))
+bot.run(os.getnv('TOKEN'))
+runrun(os.getnv('TOKEN'))
