0340c81ab58efda3077efe226a1e6ad43761c409
MissNela <44345612+MissNela@users.noreply.github.com>
2018-10-25 20:33:41 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+import time
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+	print('The bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('This message shall view only bots Owner Mrs.Nela')
+	print('----------------------------')
+
+@bot.command()
+async def ping():
+	await bot.say(':ping_pong: Why are you trying to ping me??')
+	await bot.say('Dont try it.. You dont have any chance. Heheh')
+  
+client.run(os.getnv('TOKEN'))
