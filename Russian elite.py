345367c3043206151653fd8392989753ed7aa840
MissNela <44345612+MissNela@users.noreply.github.com>
2018-10-25 15:05:22 +0200

    Create Russian elite.py

---
+++ Russian elite.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+	print('The bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('This message shall view only bots Owner Mrs.Nela')
+	print('----------------------------')
+
+@bot.command()
+async def ping():
+	await bot.say(':ping_pong: Why are you trying to ping me??')
+	await bot.say('Dont try it.. You dont have any chance. Heheh')
+		
+bot.run('NTAzMzgxMDM5ODcyMjEyOTky.Dq2JtA.b6b4lSkwcV9_-e8p1wFyH8nZwtE')
