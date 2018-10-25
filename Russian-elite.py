b1e35fa6dcf4ff65870d7b253342ada7db46faf9
MissNela <44345612+MissNela@users.noreply.github.com>
2018-10-25 18:34:00 +0200

    Update Russian-elite.py

--- Russian-elite.py
+++ Russian-elite.py
@@ -94,4 +94,4 @@ async def poll(ctx, question, *options: str):
         embed.set_footer(text='Poll ID: {}'.format(react_message.id))
         await client.edit_message(react_message, embed=embed)
 
-bot.login(process.env.token);
+bot.run(NTAzMzgxMDM5ODcyMjEyOTky.Dq2JtA.b6b4lSkwcV9_-e8p1wFyH8nZwtE);
