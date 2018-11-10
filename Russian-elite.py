6111bc5772942ab2b31db49cadf4e9f3ec04c300
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-10 19:54:43 +0100

    Update Russian-elite.py

--- Russian-elite.py
+++ Russian-elite.py
@@ -281,6 +281,12 @@ async def clear(ctx, amount = 10):
     await client.say('Messages deleted.')
 
 
+@client.command(pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def warn(ctx, userName: discord.User, *, message:str): 
+    await client.send_message(userName, "You have been warned for: **{}**".format(message))
+    await client.say(":warning: __**{0} Has Been Warned!**__ :warning: ** Reason:{1}** ".format(userName,message))
+    pass
 
 
 
