e77005c59b8015c9e0f45239ecedd63fcfd7910e
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-12 18:45:27 +0100

    Update Russian-elite.py

--- Russian-elite.py
+++ Russian-elite.py
@@ -12,7 +12,8 @@ import os
 import time
 from discord.voice_client import VoiceClient
 from discord import Game, Embed, Color, Status, ChannelType
-
+import youtube_dl
+from youtube_dl import ytdl
 
 
 
